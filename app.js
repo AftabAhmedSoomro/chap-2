72bbe4b3ead0e37dd72cd2f39a5e3d060054e57a
AftabAhmedSoomro <154562779+AftabAhmedSoomro@users.noreply.github.com>
2024-03-07 02:37:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var userName;
+
+var myName = "Aftab Ahmed Soomro";
+
+var message = "Hello World";
+alert(message);
+
+// Biodata
+var name = "Aftab Ahmed Soomro" ;
+var age= "19 Years Old";
+var course= "Web & App Development"
+alert(name);
+alert(age);
+alert(course);
+
+var pizza= "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+
+// Concatenation String
+var email= "aftab.ahmedsoomrokhan@gmail.com";
+alert("My email address is " + email);
+
+var book= "I am trying to learn from the book A smarter way to learn JavaScript";
+alert(book);
+
+var ability= "Yah! I can write HTML content through JavaScript";
+document.write(ability);
+
+specialDesign= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialDesign);
+document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
